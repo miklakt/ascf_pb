22353dab54f2f1476408e38fe4d087088eeaa18f
ml <miklakt@gmail.com>
2021-08-29 19:21:54 +0300

    profiles factory

--- test_pore.py
+++ test_pore.py
@@ -1,23 +1,14 @@
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
-from ascf_pb.solver import Phi
-from ascf_pb.topology import pore
-from ascf_pb.topology import kappa
-from ascf_pb.profiles import build_phi_profile_solver
-import matplotlib.pyplot as plt
-def get_pore_phi_profile(N, sigma, chi, pore_Radius):
-    phi_profile = build_phi_profile_solver(
-        kappa.kappa_plain, pore.D_universal, pore.phi_D_universal,
-        chi = chi, N=N, sigma = sigma, pore_Radius = pore_Radius, R = pore_Radius)
-    return phi_profile
+from ascf_pb import profile_factory, topology
 def draw_profile(phi_profile, pore_Radius):
-    phi = [phi_profile[0](z) for z  in range(pore_Radius)]
+    phi = [phi_profile(z) for z  in range(pore_Radius)]
     plt.margins(0,0)
     plt.xlim([0, pore_Radius])
-    plt.ylim([0, phi_profile[0](0)])
+    plt.ylim([0, phi_profile(0)])
     plt.plot(phi)
-def draw_pore(phi_profile, pore_Radius):
+def draw_pore(phi_profile, pore_Radius, D):
     import matplotlib.pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D
     from matplotlib.patches import Rectangle
@@ -28,7 +19,7 @@ def draw_pore(phi_profile, pore_Radius):
     rad = np.arange(pore_Radius)
     azm = np.linspace(0, 2 * np.pi, 100)
     r, th = np.meshgrid(rad, azm)
-    z = [[phi_profile[0](pore_Radius-i) for i in j] for j in r]
+    z = [[phi_profile(pore_Radius-i) for i in j] for j in r]
 
     polar_ax = plt.subplot(projection="polar")
     polar_ax.pcolormesh(th, r, z, rasterized = True, vmin = 0, vmax=0.3)
@@ -36,7 +27,7 @@ def draw_pore(phi_profile, pore_Radius):
     polar_ax.set_xticks([])
     polar_ax.set_yticks([])
 
-    channel_R = pore_Radius - phi_profile[1]
+    channel_R = pore_Radius - D
     if channel_R>0:
         polar_ax.plot(azm,[channel_R]*len(azm), color = 'red')
 
@@ -46,9 +37,10 @@ chi = 0.0
 sigma = 0.02
 N=1000
 pore_Radius = 200
-phi_profile = get_pore_phi_profile(N,sigma, chi, pore_Radius)
+phi_profile = profile_factory.phi(topology='pore',N=N,sigma=sigma, chi=chi, pore_Radius=pore_Radius, R = pore_Radius)
+D = profile_factory.D(topology='pore',N=N,sigma=sigma, chi=chi, pore_Radius=pore_Radius, R = pore_Radius)
 draw_profile(phi_profile,pore_Radius)
-fig = draw_pore(phi_profile, pore_Radius)
+fig = draw_pore(phi_profile, pore_Radius, D)
 fig.savefig(f'pore_figs/N_{N}_sigma_{sigma}_chi_{chi}_Radius_{pore_Radius}.pdf')
 
 # %%
